8f19b05ed629d16d742bb8d1f53c74ec60ecd289
Raych118 <rachelmakwara@gmail.com>
2023-02-16 03:26:17 -0800

    white space

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,11 +13,11 @@ int main(void)
     long long longLongType;
     char charType;
 
-    printf("Size of a char: %zu byte(s)\n", sizeof(charType));
-    printf("Size of an int: %zu byte(s)\n", sizeof(intType));
-    printf("Size of a long int: %zu byte(s)\n", sizeof(longType));
-    printf("Size of a long long int: %zu byte(s)\n", sizeof(longLongType));
-    printf("Size of a float: %zu byte(s)\n", sizeof(floatType));
+    printf("Size of a char: %zu byte(s)", sizeof(charType));
+    printf("Size of an int: %zu byte(s)", sizeof(intType));
+    printf("Size of a long int: %zu byte(s)", sizeof(longType));
+    printf("Size of a long long int: %zu byte(s)", sizeof(longLongType));
+    printf("Size of a float: %zu byte(s)", sizeof(floatType));
     
     return (0);
 }
