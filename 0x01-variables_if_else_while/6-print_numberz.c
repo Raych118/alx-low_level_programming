d1c179016a1456f3e911362e4cea6c63f26a6531
Raych118 <rachelmakwara@gmail.com>
2023-02-17 07:15:33 -0800

    test 7

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int y;
+
+	for (y = 48 ; y < 58 ; y++)
+	{
+		putchar(y);
+	}
+	putchar('\n');
+	return (0);
+}
