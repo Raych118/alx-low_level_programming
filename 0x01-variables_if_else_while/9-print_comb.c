2101e08feeea96184a62ab6af7c90de7d3aa6cbe
Raych118 <rachelmakwara@gmail.com>
2023-02-17 12:40:59 -0800

    combo

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,14 +9,15 @@
 int main(void)
 {
 	int num;
+	
 	for (num = 0 ; num < 10 ; num++)
 	{
 		putchar((num % 10) + '0');
 		if (num != 9)
-				{
-					putchar(',');
-					putchar(' ');
-				}
+		{
+			putchar(',');
+			putchar(' ');
+		}
 	}
 	putchar('\n');
 	return (0);
