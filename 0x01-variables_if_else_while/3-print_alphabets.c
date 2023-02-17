162b1cd60fdafceedca13521c1dcc431618dc806
Raych118 <rachelmakwara@gmail.com>
2023-02-17 06:09:32 -0800

    test 4

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,16 +8,16 @@
  */
 int main(void)
 {
-        int character;
-
-        for (character = 'a' ; character <= 'z' ; character++)
-        {
-                putchar(character);
-        }
+	int character;
+	
+	for (character = 'a' ; character <= 'z' ; character++)
+	{
+		putchar(character);
+	}
 	for (character = 'A' ; character <= 'Z' ; character++)
-        {
-                putchar(character);
-        }
-        putchar('\n');
-        return (0);
+	{
+		putchar(character);
+	}
+	putchar('\n');
+	return (0);
 }
