70407b13c47fa4681dc2428891e7b2a5078c1de1
Raych118 <rachelmakwara@gmail.com>
2023-02-17 06:05:01 -0800

    test 4

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+        int character;
+
+        for (character = 'a' ; character <= 'z' ; character++)
+        {
+                putchar(character);
+        }
+	for (character = 'A' ; character <= 'Z' ; character++)
+        {
+                putchar(character);
+        }
+        putchar('\n');
+        return (0);
+}
