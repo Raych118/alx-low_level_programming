74b557414beb09b1692a9cbfa039b2cad7dea40c
Raych118 <rachelmakwara@gmail.com>
2023-02-21 11:07:53 -0800

    test 5

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <ctype.h>
+#include <stdlib.h>
+/* more headers go there */
+
+/**
+ * _islower - Entry point
+ *
+ * Return: 1 if c is a letter or 0 if c is not a letter (Success)
+ *
+ * @c: is a variable going to be passed by whichever class calls the function
+ */
+int _isalpha(int c)
+{
+	if (isalpha(c))
+		return (1);
+	else
+		return (0);
+}
