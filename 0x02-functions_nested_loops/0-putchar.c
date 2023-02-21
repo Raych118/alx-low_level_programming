33253128c436cb741a73a83b3ce3f0f76d1f004d
Raych118 <rachelmakwara@gmail.com>
2023-02-21 09:38:45 -0800

    test 1

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include "_putchar.h"
+#include <stdio.h>
+#include <stdlib.h>
+/* more headers goes there */
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char *p = "_putchar";
+
+	while (*p)
+	{
+		_putchar(*p);
+		p++;
+	}
+	_putchar('\n');
+	return (0);
+}
