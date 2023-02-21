22f58b9ad39e7f183a83dc57868c91738c3eec8a
Raych118 <rachelmakwara@gmail.com>
2023-02-21 14:10:36 -0800

    Test 10

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,43 @@
+#include <stdlib.h>
+#include "main.h"
+#include <stdio.h>
+/* more headers go there */
+
+/**
+ * times_table - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+void times_table(void)
+{
+	int tr, br, prod, print;
+	int after;
+
+	for (tr = 0 ; tr < 10 ; tr++)
+	{
+		for (br = 0 ; br < 10 ; br++)
+		{
+			prod = tr * br;
+
+			if (prod >= 10)
+			{
+				print = prod / 10;
+				_putchar(print + '0');
+				after = prod % 10;
+				_putchar(after + '0');
+			}
+			else
+			{
+				_putchar(' ');
+				_putchar(prod + '0');
+			}
+
+			if (br != 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+	}
+}
