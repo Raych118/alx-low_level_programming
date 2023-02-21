82948fb53fc4648c3a9adf43fd0581ff50cc30e2
Raych118 <rachelmakwara@gmail.com>
2023-02-21 11:43:34 -0800

    test 7

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <ctype.h>
+#include <stdlib.h>
+/* more headers go there */
+
+/**
+ * _abs - Entry point
+ *
+ * Return: Always return 0 (Success)
+ *
+ * @abs: a variable to be passed by the calling function
+ */
+int _abs(int abso)
+{
+	return (abs(abso));
+}
