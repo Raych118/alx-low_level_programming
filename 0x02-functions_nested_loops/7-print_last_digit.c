46202366ff196fb39330d041ae6164ff673905de
Raych118 <rachelmakwara@gmail.com>
2023-02-21 12:09:29 -0800

    test 8

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ * print_last_digit - Entry point
+ *
+ * Return: Always last digit (Success)
+ *
+ * @n: variable to be passed by calling function
+ */
+int print_last_digit(int n)
+{
+	int dig;
+	
+	dig = n % 10;
+
+	if (dig < 0)
+		dig *= -1;
+
+	_putchar(dig + '0');
+	return (dig);
+}
