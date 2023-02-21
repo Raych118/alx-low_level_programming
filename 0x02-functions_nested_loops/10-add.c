f437465d2d75c1e2c8f8fcb574fa7a7cf2a252d7
Raych118 <rachelmakwara@gmail.com>
2023-02-21 14:35:07 -0800

    Test 11

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <ctype.h>
+#include <stdlib.h>
+/* more headers go there */
+
+/**
+ * _add - Entry point
+ *
+ * Return: Always return 0 (Success)
+ *
+ * @a1: a variable to be passed by the calling function
+ *
+ * @a2: a variable to be passed.
+ */
+int add(int a1, int a2)
+{
+	return (a1 + a2);
+}
