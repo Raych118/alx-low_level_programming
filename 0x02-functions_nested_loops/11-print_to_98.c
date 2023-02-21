39fc4199282f3b8642aa7da294b8f16db13c73d9
Raych118 <rachelmakwara@gmail.com>
2023-02-21 15:14:18 -0800

    test 12

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,46 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/* more headers go there */
+
+/**
+ * print_to_98 - Entry point
+ *
+ * Return: Always return 0 (Success)
+ *
+ * @n: a variable to be passed by the calling function
+ */
+void print_to_98(int n)
+{
+	int x;
+
+	if (n <= 98)
+	{
+		for (x = n ; x <= 98 ; x++)
+		{
+			if (x != 98)
+                        {
+                                printf("%d, ", x);
+                        }
+                        else
+                        {
+                                printf("%d", x);
+                        }
+		}
+	}
+	else 
+	{
+		for (x = n ; x >= 98 ; x--)
+		{
+			if (x != 98)
+			{
+				printf("%d, ", x);
+			}
+			else
+			{
+				printf("%d", x);
+			}
+		}
+	}
+	printf("\n");
+}
