f253feeb63d180ed5ccb097fa4e3f69b52ca4f0f
Raych118 <rachelmakwara@gmail.com>
2023-02-23 06:54:45 -0800

    test

--- 0x03-debugging/3-main_a.c
+++ 0x03-debugging/3-main_a.c
@@ -14,7 +14,7 @@ int main(void)
     int year;
 
     month = 2;
-    day = 28;
+    day = 29;
     year = 2424;
 
     printf("Date: %02d/%02d/%04d\n", month, day, year);
--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -12,7 +12,7 @@
 
 void print_remaining_days(int month, int day, int year)
 {
-	if (year % 4 != 0 && month >= 2 && day >= 31)
+	if (year % 4 != 0 && month)
 	{
 		if (month == 2 && day >= 60)
 		{
@@ -24,7 +24,7 @@ void print_remaining_days(int month, int day, int year)
 			printf("Remaining days: %d\n", 365 - day);
 		}
 	}
-	else if (year % 4 == 0 && month >= 2 && day >= 31)
+	else if (year % 4 == 0)
 	{
 		printf("Day of the year: %d\n", day);
 		printf("Remaining days: %d\n", 366 - day);
